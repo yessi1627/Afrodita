94a9d4fb21ca7f3d2bd0e038a57a778d699beb1d
Myriam Yessenia Tique <yesse1630@gmail.com>
2025-11-29 23:00:33 -0500

    feat(productos):Agregar catalogo de productos Afrodita

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 // Array vacio
-const productos = [];
+const productos = [
+    { nombre: "Shampoo Reparador DivineCurl", tipo: "capilar", precio: 38000 },
+    { nombre: "Mascarilla Hidratante GoldenSkin", tipo: "piel", precio: 45000 },
+    { nombre: "Aceite Capilar Lovex", tipo: "capilar", precio: 52000 },
+    {nombre: "Repolarizador trigo de la mañana", tipo: "capilar", precio:87000}
+];
 
 // Función 1: mostrar mensaje de bienvenida
 function mostrarBienvenida() {

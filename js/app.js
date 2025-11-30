61161f7f4b23be314815de32abebcfcb78c4a7cf
Myriam Yessenia Tique <yesse1630@gmail.com>
2025-11-29 22:53:23 -0500

    feat(js): Creación de funciones iniciales y array inicialmente vacio

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Array vacio
+const productos = [];
+
+// Función 1: mostrar mensaje de bienvenida
+function mostrarBienvenida() {
+    alert(" Bienvenida a Afrodita \nTu belleza merece lo mejor.\nVamos a ver de que te antojas hoy.");
+}
+
+// Función 2: mostrar los productos disponibles
+function mostrarProductos() {
+    console.log(" Lista de productos disponibles (pronto se llenará)...");
+}
+
+// Función 3:  Seleccionar un producto
+function seleccionarProducto() {
+    console.log("Aquí se permitirá seleccionar un producto (lo haremos en el siguiente paso).");
+}
+
+// Invocación inicial
+mostrarBienvenida();

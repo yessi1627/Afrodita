112313dba94cc2e5db4de398557bcd784ba18087
Myriam Yessenia Tique <yesse1630@gmail.com>
2025-12-13 09:41:39 -0500

    feat(cart): estructura base del carrito de compras

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,15 @@
 
-// Array que almacenará los productos del carrito
+// Array que almacenará los productos al carro de compras
 let cart = [];
 
-// Función: agregar producto al carrito
+// Función: agregar producto al carro de compras
 function agregarAlCarrito(producto) {
   cart.push(producto);
   console.log("Producto agregado al carrito:", producto);
   console.log("Carrito actual:", cart);
 }
 
-// Función: obtener carrito
+// Función: obtener carro de compras
 function obtenerCarrito() {
   return cart;
 }
